bcb47e39849e819ae58383e2a62f59fa9abae6a9
alban vervust <alban.vervust@gmail.com>
2012-12-12 15:45:33 +0100

    premiere utilisation de git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PremiereApp::Application.config.secret_token = '0eea34e5711449016e346af2bd67d304c6f4e33a2a53cac5bd5fe459093852e83d40b02e7e6aeb2fc4667ef4b142d077ff07af1f013832b3d76bede27502b264'
